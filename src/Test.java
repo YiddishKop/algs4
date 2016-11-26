16f2453cc25df34c57eee4723bb7535ff168704c
yiddishkop <yiddishkop@163.com>
2016-11-26 10:50:31 +0800

    algorithms code for algs4

---
+++ src/Test.java
@@ -0,0 +1,15 @@
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class Test {
+	public static void main(String[] args) {
+		StdOut.println("hello world");
+		System.out.println(2.0e-6 * 100000000.1);
+		System.out.println((0 + 15) / 2);
+		System.out.println(true && false || true && true);
+		System.out.println(1 + "e");
+		String x = 2.3 + "3";
+		System.out.println(x);
+	}
+
+}
